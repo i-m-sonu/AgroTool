8be29f0db5bd9511adffd402ba2a65253a2cf661
Sonu Kumar <sonukumar@Sonus-MacBook-Air.local>
2024-02-01 19:38:00 +0530

    dataset added

---
+++ app/pages/Card.js
@@ -0,0 +1,20 @@
+const Card = (props) => {
+  return (
+    <>
+      <div className="card">
+        <br />
+        <p>{props.m}</p>
+        <p>{props.a}</p>
+        <p>{props.b}</p>
+        <p>{props.c}</p>
+        <p>{props.d}</p>
+        <p>{props.e}</p>
+        <p>{props.f}</p>
+        <p>{props.g}</p>
+        <p>{props.h}</p>
+      </div>
+    </>
+  );
+};
+
+export default Card;
